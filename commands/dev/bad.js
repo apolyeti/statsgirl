dc5b9d7557fad44aa6540585b9d7067bc597c44e
apolyeti <azhandarveen@gmail.com>
2023-04-23 19:43:18 -0700

    add embeds to good and bad

--- commands/dev/bad.js
+++ commands/dev/bad.js
@@ -3,24 +3,24 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('good')
-        .setDescription("for all the good things you've done as a person")
+        .setName('bad')
+        .setDescription("for doing something that negatively affected yourself or others")
         .addStringOption(option =>
             option.setName('act')
-                .setDescription('whatever good thing you did')
+                .setDescription('whatever bad thing you did')
                 .setRequired(true))
         .addIntegerOption(option =>
             option.setName('points')
-                .setDescription('how many points you think you deserve for being better')
+                .setDescription('how many points you think you deserve to lose')
                 .setRequired(true)),
         
     async execute(interaction) {
         const sendEmbed = new EmbedBuilder()
         // whats the hex color for green
-            .setColor('#54d171')
-            .setTitle('good!')
-            .setDescription(`${interaction.user.username} did a good thing: ${interaction.options.getString('act')} 
-            points deserved: ${interaction.options.getInteger('points')}`)
+            .setColor('#d62d4f')
+            .setTitle('bad :(')
+            .setDescription(`${interaction.user.username} did a bad thing: ${interaction.options.getString('act')} 
+            points lost: ${interaction.options.getInteger('points')}`)
             .setImage(interaction.user.avatarURL())
         await interaction.reply({ embeds: [sendEmbed] })
     },
