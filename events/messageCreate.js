3615ddaf5ebb1b087be7f638d99440a2c538c2b8
apolyeti <azhandarveen@gmail.com>
2023-08-13 20:21:52 -0700

    ocr only works when bot mentioned

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -27,49 +27,52 @@ module.exports = {
         }
 
 
-
-        if (message.attachments) {
-            console.log('attachment found, sent by ' + message.author.username + ' in #' + message.channel.name);
-            message.attachments.forEach(attachment => {
-                console.log(attachment.url);
-                let data = new FormData();
-                data.append('language', 'jpn');
-                data.append('isOverlayRequired', 'false');
-                data.append('url', attachment.url);
-                data.append('iscreatesearchablepdf', 'false');
-                data.append('issearchablepdfhidetextlayer', 'false');
-
-                const config = {
-                    method: 'post',
-                    maxBodyLength: Infinity,
-                    url: 'https://api.ocr.space/parse/image',
-                    headers: { 
-                      'apikey': ocr_key, 
-                      ...data.getHeaders()
-                    },
-                    data : data
-                };
-                ax(config)
-                .then(function (response) {
-                    const original = response.data.ParsedResults[0].ParsedText;
-                    if (original.length < 5) return;
-                    const data = new URLSearchParams();
-                    data.append('text', original);
-                    data.append('target_lang', 'EN');
-                    axios.post('/translate', data)
-                        .then((response) => {
-                            if (response.data.translations[0].text.length < 5) return;
-                            message.channel.send(`Original: ${original}\nTranslated: ${response.data.translations[0].text}`);
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                        });
+        if (message.content.startsWith('<@1095069321631387838>')) {
+            if (message.attachments) {
+                console.log('attachment found, sent by ' + message.author.username + ' in #' + message.channel.name);
+                message.attachments.forEach(attachment => {
+                    console.log(attachment.url);
+                    let data = new FormData();
+                    data.append('language', 'jpn');
+                    data.append('isOverlayRequired', 'false');
+                    data.append('url', attachment.url);
+                    data.append('iscreatesearchablepdf', 'false');
+                    data.append('issearchablepdfhidetextlayer', 'false');
+    
+                    const config = {
+                        method: 'post',
+                        maxBodyLength: Infinity,
+                        url: 'https://api.ocr.space/parse/image',
+                        headers: { 
+                          'apikey': ocr_key, 
+                          ...data.getHeaders()
+                        },
+                        data : data
+                    };
+                    ax(config)
+                    .then(function (response) {
+                        const original = response.data.ParsedResults[0].ParsedText;
+                        if (original.length < 5) return;
+                        const data = new URLSearchParams();
+                        data.append('text', original);
+                        data.append('target_lang', 'EN');
+                        axios.post('/translate', data)
+                            .then((response) => {
+                                if (response.data.translations[0].text.length < 5) return;
+                                message.channel.send(`Original: ${original}\nTranslated: ${response.data.translations[0].text}`);
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            });
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
                 })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            })
+            }
         }
+
+
         let messageLanguage;
         try {
             const result = await cld.detect(message.content);
